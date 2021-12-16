7fe58d80a6f36d53bcb4c3ebc35a34d6ddb506cb
Pavlov Nickolay <pavlov.n98@yandex.ru>
2021-12-16 08:55:52 +0300

    collected static

--- booking_spaces/booking_spaces/settings.py
+++ booking_spaces/booking_spaces/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@71flj5_6r11#pty()e_k&wphnnxm1(r!12^*a)*ev=1!#cjrz'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
